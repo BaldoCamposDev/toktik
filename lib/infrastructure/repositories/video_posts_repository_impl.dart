5014f9e81aef9ef0c47bbd54e7bcbec7edadc0f7
BaldoCampos <baldocamposg@gmail.com>
2026-04-02 15:20:06 -0700

    refactor: :recycle: Added datasource and repository layers

---
+++ lib/infrastructure/repositories/video_posts_repository_impl.dart
@@ -0,0 +1,19 @@
+import 'package:toktik/domain/datasources/video_posts_datasource.dart';
+import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/domain/repositories/video_post_repository.dart';
+
+class VideoPostsRepository implements VideoPostRepository {
+  final VideoPostDataSource videosDatasource;
+
+  VideoPostsRepository({required this.videosDatasource});
+
+  @override
+  Future<List<VideoPost>> getFavoriteVideosByUser(int userId) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<VideoPost>> getTrendingVideosByPage(int page) {
+    return videosDatasource.getTrendingVideosByPage(page);
+  }
+}
